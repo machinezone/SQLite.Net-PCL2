17515e3d06e9e906540dda68a7e818641bc7fad4
Garth Gillespie <garth@mz.com>
2024-05-24 18:45:40 -0700

    Switched back to the official SQLitePCLRaw.

--- SQLite.Net.Tests/SerializeTest.cs
+++ SQLite.Net.Tests/SerializeTest.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using NUnit.Framework;
-
-using System.Threading.Tasks;
 
 namespace SQLite.Net2.Tests
 {
@@ -32,7 +31,7 @@ namespace SQLite.Net2.Tests
         }
 
         [Test]
-        public async Task SerializeRoundTrip()
+        public void SerializeRoundTrip()
         {
             var obj1 = new SerializeTestObj
             {
@@ -41,7 +40,7 @@ namespace SQLite.Net2.Tests
 
             SQLiteConnection srcDb = new SerializeTestDb(":memory:");
 
-            int numIn1 = srcDb.Insert(obj1);
+            var numIn1 = srcDb.Insert(obj1);
             Assert.AreEqual(1, numIn1);
 
             List<SerializeTestObj> result1 = srcDb.Query<SerializeTestObj>("select * from SerializeTestObj").ToList();
@@ -49,11 +48,47 @@ namespace SQLite.Net2.Tests
             Assert.AreEqual(obj1.Text, result1.First().Text);
 
 
-            byte[] serialized = srcDb.Serialize();
+            var serialized = srcDb.Serialize();
             srcDb.Close();
 
             SQLiteConnection destDb = new SerializeTestDb(":memory");
             destDb.Deserialize(serialized);
+
+            result1 = destDb.Query<SerializeTestObj>("select * from SerializeTestObj").ToList();
+            Assert.AreEqual(numIn1, result1.Count);
+            Assert.AreEqual(obj1.Text, result1.First().Text);
+
+            destDb.Close();
+        }
+
+        [Test]
+        public void SerializeRoundTripStreams()
+        {
+            var obj1 = new SerializeTestObj
+            {
+                Text = "GLaDOS loves testing!"
+            };
+
+            SQLiteConnection srcDb = new SerializeTestDb(":memory:");
+
+            var numIn1 = srcDb.Insert(obj1);
+            Assert.AreEqual(1, numIn1);
+
+            List<SerializeTestObj> result1 = srcDb.Query<SerializeTestObj>("select * from SerializeTestObj").ToList();
+            Assert.AreEqual(numIn1, result1.Count);
+            Assert.AreEqual(obj1.Text, result1.First().Text);
+
+
+            MemoryStream stream = new();
+            var size = srcDb.Serialize(stream);
+            Assert.That(size, Is.GreaterThan(0));
+            srcDb.Close();
+
+            stream.Seek(0, SeekOrigin.Begin);
+
+            SQLiteConnection destDb = new SerializeTestDb(":memory");
+            destDb.Deserialize(stream);
+            Assert.That(stream.Position, Is.EqualTo(size));
 
             result1 = destDb.Query<SerializeTestObj>("select * from SerializeTestObj").ToList();
             Assert.AreEqual(numIn1, result1.Count);
